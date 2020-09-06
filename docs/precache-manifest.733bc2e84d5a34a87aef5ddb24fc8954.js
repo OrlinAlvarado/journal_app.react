11ef97ae293407d98f4e40d34e399a6d3fad5d70
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-09-05 23:02:02 -0600

    Agregando carpeta docs para publicacion en pagina de github

---
+++ docs/precache-manifest.733bc2e84d5a34a87aef5ddb24fc8954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cdc3d30e8b20bf6f5f156bc5aec4ab1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fdc7b7d34302ba2e58b",
+    "url": "/static/css/main.69bd1452.chunk.css"
+  },
+  {
+    "revision": "430ffa406c58dd845e0d",
+    "url": "/static/js/2.70cba36e.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.70cba36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fdc7b7d34302ba2e58b",
+    "url": "/static/js/main.4914172b.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
